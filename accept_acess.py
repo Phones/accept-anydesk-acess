c2bebc682191a9f715d2f85ce5fb0f8cb2aeace4
paulo.campos <paulo.campos@minervafoods.com>
2023-08-10 23:31:59 -0300

    função para verificar se um programa em execução

--- accept_acess.py
+++ accept_acess.py
@@ -1,14 +1,24 @@
+import psutil
 import pyautogui
+import subprocess
 from time import sleep
 
-def search_and_click(image_path):
+def search_and_click(image_name):
+    # caminho para a imagem
+    image_path = f"images/{image_name}"
     # Get the position of the button
     button_position = pyautogui.locateCenterOnScreen(image_path)
     # Click on the button
     pyautogui.click(button_position)
 
-while True:
-    image_path = "images\\teste.png"
-    search_and_click(image_path)
-    
-    sleep(30)
+def is_program_running(program):
+    for proc in psutil.process_iter(['pid', 'name']):
+        if program in proc.info['name']:
+            return True
+        
+    return False
+
+def is_anydesk_running_and_start():
+    if not is_program_running("anydesk"):
+        subprocess.Popen("anydesk", shell=True)
+        print("O AnyDesk não está em execução.")
