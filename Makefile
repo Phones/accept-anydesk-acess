ca237d1487bca31519d55652313879ecfa5ab975
paulo.campos <paulo.campos@minervafoods.com>
2023-08-16 03:17:24 -0300

    Makefile, get_credentials, requiriments atrualizado, black para formatar o codigo e codigo formatado com o black

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	python accept_acess.py
+
+format:
+	black accept_acess.py
--- accept_acess.py
+++ accept_acess.py
@@ -1,8 +1,10 @@
 import os
+import yaml
 import psutil
 import pyautogui
 import subprocess
 from time import sleep
+from yaml.loader import SafeLoader
 from datetime import datetime, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -16,19 +18,33 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 
 dict_period_convert = {
-    1 : 13,
-    2 : 14,
-    3 : 15,
-    4 : 16,
-    5 : 17,
-    6 : 18,
-    7 : 19,
-    8 : 20,
-    9 : 21,
-    10 : 22,
-    11 : 23,
-    12 : 24
+    1: 13,
+    2: 14,
+    3: 15,
+    4: 16,
+    5: 17,
+    6: 18,
+    7: 19,
+    8: 20,
+    9: 21,
+    10: 22,
+    11: 23,
+    12: 24,
 }
+
+
+def get_credentials():
+    """Carrega as credenciais do arquivo credentials.yml
+
+    Returns:
+        dict: Retorna um dicionario com as credenciais
+    """
+    yml_to_dict = {}
+    with open(f"{os.getcwd()}/conf/credentials.yml", "r") as credentials_file:
+        yml_to_dict = yaml.load(credentials_file, Loader=SafeLoader)
+
+    return yml_to_dict
+
 
 def convert_str_to_datetime(hora_str):
     try:
@@ -38,20 +54,22 @@ def convert_str_to_datetime(hora_str):
     except ValueError:
         print("Formato de hora inválido. Use o formato HH:MM:SS.")
 
+
 def acess_yopemail():
     # Abra o navegador
     driver.get("https://yopmail.com/")
-    
+
     # Pega o elemento de acesso da caixa de inserir email
     input_email_box = wait_element_using_id("login", 10)
 
-    #insere o email
-    email = ""
+    # insere o email
+    email = get_credentials()["e-mail"]
     input_email_box.send_keys(email)
 
     # Siomula um enter
     input_email_box.send_keys(Keys.ENTER)
-  
+
+
 def wait_element_using_id(id, time_limit=10):
     # Cria um objeto de espera
     wait = WebDriverWait(driver, time_limit)
@@ -62,29 +80,37 @@ def wait_element_using_id(id, time_limit=10):
     # Esperar até que o elemento com o ID desejado esteja presente
     insert_email_box = wait.until(EC.presence_of_element_located((By.ID, id)))
 
-    return insert_email_box 
+    return insert_email_box
+
 
 def wait_element_using_xpath(xpath, time_limit=10):
     # Espere até que o elemento com o XPath completo seja carregado
-    WebDriverWait(driver, time_limit).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    WebDriverWait(driver, time_limit).until(
+        EC.presence_of_element_located((By.XPATH, xpath))
+    )
 
     # Pegue o texto do elemento
     search_box = driver.find_element(By.XPATH, xpath)
 
     return search_box
 
+
 def wait_element_using_class_name(class_name, time_limit=10):
     # Espere até que o elemento com o XPath completo seja carregado
-    WebDriverWait(driver, time_limit).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
+    WebDriverWait(driver, time_limit).until(
+        EC.presence_of_element_located((By.CLASS_NAME, class_name))
+    )
 
     # Pegue o texto do elemento
     search_box = driver.find_element_by_class_name(class_name)
 
     return search_box
 
+
 def get_text_of_last_email():
     element = wait_element_using_id("mail", 10)
     return element.text
+
 
 def get_hour_of_email():
     # Pega o elemento que contem a hora do email
@@ -92,16 +118,19 @@ def get_hour_of_email():
     hour_element = wait_element_using_xpath(xpath)
 
     return hour_element.text
-    
+
+
 def switch_to_iframe_using_xpath(xpath):
     iframe_element = wait_element_using_xpath(xpath, 10)
 
     driver.switch_to.frame(iframe_element)
 
+
 def switch_to_iframe_using_id(id):
     iframe_element = wait_element_using_id(id, 15)
 
     driver.switch_to.frame(iframe_element)
+
 
 def search_and_click(image_name):
     # caminho para a imagem
@@ -124,17 +153,20 @@ def search_and_click(image_name):
     # Click on the button
     pyautogui.click(button_position)
 
+
 def is_program_running(program):
-    for proc in psutil.process_iter(['pid', 'name']):
-        if program in proc.info['name']:
+    for proc in psutil.process_iter(["pid", "name"]):
+        if program in proc.info["name"]:
             return True
-        
+
     return False
+
 
 def is_anydesk_running_and_start():
     if not is_program_running("anydesk"):
         subprocess.Popen("anydesk", shell=True)
         print("O AnyDesk não está em execução.")
+
 
 def using_images_to_search(program):
     # Program (anydesk or teamviewer or all)
@@ -146,6 +178,7 @@ def using_images_to_search(program):
         elif program in "all":
             search_and_click(image_name=image)
 
+
 def get_time_and_period_AM_PM():
     # Pega a hora do e-mail e separa em uma lista
     email_hour_list = get_hour_of_email().split(" ")
@@ -154,7 +187,7 @@ def get_time_and_period_AM_PM():
     period = email_hour_list[-1]
     # Pega a hora
     hour, minutes, seconds = [int(x) for x in email_hour_list[-2].split(":")]
-    
+
     # Se for PM ajusta para o formato 24h
     if "PM" in period:
         hour = dict_period_convert[hour]
@@ -165,18 +198,23 @@ def get_time_and_period_AM_PM():
     # Pega a hora atual é converte em datetime para que possa ser subtraida da outra data
     h_m_s_now = datetime.now().strftime("%H:%M:%S")
     hour_now_list = h_m_s_now.split(":")
-    hour_now, minutes_now, seconds_now = [int(x) for x in hour_now_list]  
+    hour_now, minutes_now, seconds_now = [int(x) for x in hour_now_list]
     hour_now_datetime = datetime(2023, 1, 1, hour_now, minutes_now, seconds_now)
-    
+
     # Calcula a diferença entre a hora do email para a hora atual
-    diference_in_minutes = (hour_now_datetime - email_hour_datetime).total_seconds() / 60
+    diference_in_minutes = (
+        hour_now_datetime - email_hour_datetime
+    ).total_seconds() / 60
     diference_in_minutes = round(diference_in_minutes, 2)
-    print(f"| Hora atual --> [{h_m_s_now}] Diference --> [{abs(diference_in_minutes)}]    |")
+    print(
+        f"| Hora atual --> [{h_m_s_now}] Diference --> [{abs(diference_in_minutes)}]    |"
+    )
     if abs(diference_in_minutes) <= 5:
         print("| ---> O e-mail foi enviado dentro dos ultimos 5 min |")
         return True
-    
+
     return False
+
 
 acess_yopemail()
 while True:
@@ -184,8 +222,8 @@ while True:
     try:
         driver.refresh()
         switch_to_iframe_using_id("ifmail")
-        
-        # Verifica se o anydesk est aem execução. Caso não, inicia 
+
+        # Verifica se o anydesk est aem execução. Caso não, inicia
         is_anydesk_running_and_start()
         print("------------------------------------------------------")
         if get_time_and_period_AM_PM():
@@ -193,7 +231,8 @@ while True:
             if "start" in email_text:
                 using_images_to_search("anydesk")
         print("------------------------------------------------------")
-        
+
         sleep(300)
     except Exception as ERRO:
         print(f"ERRO: {ERRO}")
+        sleep(600)
